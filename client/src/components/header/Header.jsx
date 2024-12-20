109a0a65d0d8dda199e885e6aa9e2e69cbaddf22
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-12-20 09:26:36 +0530

    Navbar styling and header with style and react-date-range implemented with routing

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,76 @@
+import "./header.css";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { faPerson } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faHotel,faPlane,faCar,faLocationDot,faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { useState } from "react";
+const Header = () => {
+    const [date, setDate] = useState([
+        {
+          startDate: new Date(),
+          endDate: null,
+          key: 'selection'
+        }
+      ]);
+  return (
+    <div className="header">
+        <div className="headerContainer">
+            <div className="headerList">
+                <div className="headerListItem active">
+                    <FontAwesomeIcon icon={faHotel} />
+                    <span>Stays</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlane} />
+                    <span>Flights</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faCar} />
+                    <span>Car rentals</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faLocationDot} />
+                    <span>Attractions</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faTaxi} />
+                    <span>Airport taxis</span>
+                </div>
+            </div>
+            <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+            <p className="headerDesc">
+                Get rewarded for your travels – unlock instant savings of 10% or
+                more with a free Lamabooking account
+            </p>
+            <button className="headerBtn">Sign in/Register</button>
+            <div className="headerSearch">
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faHotel} className="headerIcon"/>
+                    <input type="text" placeholder="Where are you going?" className="headerSearchInput"/>
+                </div>
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
+                    <span className="headerSearchText">check in date to check out date</span>
+                    <DateRange
+                        editableDateInputs={true}
+                        onChange={item => setDate([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        className="date"
+                    />
+                </div>
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
+                    <span className="headerSearchText">2 adults 2 children 1 room</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default Header;
---
+++ client/src/pages/list/List.jsx
@@ -0,0 +1,8 @@
+import "./list.css"
+const List = () => {
+  return (
+    <div className="List">List</div>
+  )
+};
+
+export default List;
