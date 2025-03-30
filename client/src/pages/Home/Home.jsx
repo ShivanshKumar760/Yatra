05016104dd76c2c8bb679a49018282d5aaf09f61
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2025-03-30 15:52:40 +0530

    navbar component error fixed-1

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import "./home.css";
-import Navbar from "../../components/Navbar/Navbar";
+import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList";
